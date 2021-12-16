54978972232fef9a09618092bc9ff87a5da43262
David Zhuang <dvdzhuang@gmail.com>
2021-12-16 15:22:02 -0500

    median

--- stats/stats.py
+++ stats/stats.py
@@ -7,12 +7,15 @@ times = []
 for _ in range(n):
     times.append(float(f.readline()))
 
+times.sort()
+median = (times[49] + times[50]) / 2
 mean = sum(times) / n
 std = math.sqrt(sum([(x - mean) ** 2 for x in times]) / n)
-minimum = min(times)
-maximum = max(times)
+minimum = times[0]
+maximum = times[n-1]
 
 print('Mean:', mean)
+print('Median:', median)
 print('Standard deviation:', std)
 print('Minimum:', minimum)
 print('Maximum:', maximum)
